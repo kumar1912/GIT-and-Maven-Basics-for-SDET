2bc3365e6398e68e019d99f793d0bc80385b5c8b
kumar <kumar.dodda@gmail.com>
2020-01-22 09:15:33 +0530

    Revert "Revert "3rd Messege""
    This reverts commit 7edc3c12ff361040af98e642f6ea3ce54b8cd7fc.

--- src/main/java/mypackage/Calculator.java
+++ src/main/java/mypackage/Calculator.java
@@ -1,5 +1,8 @@
 package mypackage;
-
+//Set variables with lambda expressions
+//Set variables with lambda expressions
+//Set variables with lambda expressions
+//Set variables with lambda expressions
 @FunctionalInterface
 public interface Calculator {
    int calculate(int x, int y);
